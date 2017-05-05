6301d9fa7488bab87c0f680e0dbf6db08c40551b
Alexander Rusetski <alex.rus@core-tech.ru>
2017-05-05 21:07:39 +0300

    rename

---
+++ run.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+import encode
+import argparse
+import os
+
+from config import find_dir
+from getlist import get_list
+
+edited_dir = find_dir + 'announcers_added/'
+
+if os.path.exists(edited_dir):
+		pass
+else:
+	os.mkdir(edited_dir)
+
+def find_torrents(dir = find_dir):
+	files = [i for i in os.listdir(dir) if i[-7:] == 'torrent']
+	return files
+
+def args():
+	parser = argparse.ArgumentParser()
+	parser.add_argument('-s', help = "--search", default = False)
+	params = parser.parse_args()
+	return params
+
+
+filenames = find_torrents()
+
+
+def add_announcers(filename, dir = find_dir):
+	with open(dir + filename, 'rb') as file:
+		tdb = file.read()
+		other = encode.bdecode(tdb)
+		dct = other
+		ann_list = dct['announce-list']
+
+	announcers = get_list()
+
+	for announcer in announcers:
+		tl = []
+		tl.append(announcer)
+		ann_list.append(tl)
+
+	dct['announce-list'] = ann_list
+
+	pack_back = encode.bencode(dct)
+
+
+	with open(edited_dir + filename, 'wb') as file:
+		file.write(pack_back)
+		print('%s saved to %s' %(filename, edited_dir))
+	file.close()
+
+if __name__ == "__main__":
+	for i in filenames:
+		add_announcers(i)
