f2f216bff071454d0107d759ee844592dc97e339
Alexander Rusetski <alex.rus@core-tech.ru>
2017-05-05 22:08:37 +0300

    add comments

--- config.py
+++ config.py
@@ -1,8 +1,10 @@
 import sys
 
 if sys.platform  != 'win32':
+	# for linux users
 	torrent_dir = '/tmp/'
 else:
+	# for windows users
 	torrent_dir = 'd:\\temp\\'
 
 url = 'https://raw.githubusercontent.com/ngosang/trackerslist/master/trackers_all.txt'
