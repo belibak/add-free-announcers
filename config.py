f800bd299d5fc9622f4852b70490245d6432d915
Alexander Rusetski <alex.rus@core-tech.ru>
2017-05-05 21:25:57 +0300

    config win

---
+++ config.py
@@ -0,0 +1,8 @@
+import sys
+
+if sys.platform  != 'win32':
+	torrent_dir = '/home/white/Downloads/'
+else:
+	torrent_dir = 'd:\\temp\\'
+
+url = 'https://raw.githubusercontent.com/ngosang/trackerslist/master/trackers_all.txt'
